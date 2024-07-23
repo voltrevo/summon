b883b8e7193affdc28c3c6929fefd48a4cda0abf
Andrew Morris <voltrevo@gmail.com>
2024-07-23 19:00:03 +0900

    Add a swap

--- examples/lib/hash.ts
+++ examples/lib/hash.ts
@@ -4,6 +4,7 @@ export default function hash(salt: number, msg: number): number {
   for (let i = 0; i < 16; i++) {
     a ^= b;
     b = (b << 11) + (b >> 53) + 123456789012345;
+    [a, b] = [b, a];
   }
 
   return a + b;
